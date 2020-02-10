c91e9eafc257f42ef0b57e6aae3f577c3655fc63
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-02-09 20:46:51 -0600

    Hey there

--- my-app/src/components/SearchBar.tsx
+++ my-app/src/components/SearchBar.tsx
@@ -1,30 +1,22 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect } from 'react';
 import { useTextInput } from '../hooks/useTextInput';
-import { Context as SongsContext } from '../contexts/songs';
 import { InputProps } from '../types/Elements';
 import TextInput from './TextInput';
 
-const SearchBar: React.FC = (): JSX.Element => {
-  const { fetchTracks } = useContext(SongsContext);
+interface Props {
+  fetchTracks: Function;
+}
+
+const SearchBar = ({ fetchTracks }: Props): JSX.Element => {
   const input = useTextInput('');
 
   useEffect(() => {
-    let ignore = false;
-
-    const fetch = (): void => {
-      if (input.value && !ignore) {
-        fetchTracks(input.value);
-      }
-    };
-    fetch();
-
-    return () => {
-      ignore = true;
-    };
+    if (input.value) {
+      fetchTracks(input.value);
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [input.value]);
 
-  // const inputData = { ...input, placeholder: 'Search Dude', type: 'text' };
   const inputData: InputProps = {
     ...input,
     placeholder: 'Search dude',
--- my-app/src/screens/Home.tsx
+++ my-app/src/screens/Home.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SearchBar from '../components/SearchBar';
+import { Context as TracksContext } from '../contexts/songs';
 
 const Home: React.FC = (): JSX.Element => {
-  return <SearchBar />;
+  const { state, fetchTracks } = useContext(TracksContext);
+  console.log(state);
+  return <SearchBar fetchTracks={fetchTracks} />;
 };
 export default Home;
