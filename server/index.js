dacc8a70fbaec054082a6b2921f05e708b62723a
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-02-13 21:51:39 -0600

    I don't know what I am doing again

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const tracksRouter = require('./routes/tracks');
-const albumsRouter = require('./routes/albums');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -9,7 +8,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(tracksRouter);
-app.use(albumsRouter);
 
 app.listen(4000, () => {
   console.log('Listening...');
