8f7fbf20bb304b01881b3dd157ff4f15546068f2
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-02-21 17:55:41 -0600

    This is weird

--- my-app/src/components/player/Player.tsx
+++ my-app/src/components/player/Player.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { usePlayer } from '../../hooks/usePlayer';
-import { useHtmlElement } from '../../hooks/useHtmlElement';
 import {
   StyledPlayer,
   StyledDiv,
@@ -12,14 +11,13 @@ const TRACK_URL =
   'https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_700KB.mp3';
 
 const Player = (): JSX.Element => {
-  const [
-    playerRef,
+  const {
+    ref: playerRef,
     setIsPlaying,
     isPlaying,
     currentTime,
     duration
-  ] = usePlayer();
-  // const [divRef, width] = useHtmlElement();
+  } = usePlayer();
 
   // Current progress
   const progress = (currentTime / duration) * 100;
--- my-app/src/hooks/usePlayer.ts
+++ my-app/src/hooks/usePlayer.ts
@@ -1,15 +1,24 @@
 import { useCallback, useState } from 'react';
 
-export const usePlayer = () => {
+interface Data {
+  ref: any;
+  setIsPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+  isPlaying: boolean;
+  currentTime: number;
+  duration: number;
+}
+
+export const usePlayer = (): Data => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [duration, setDuration] = useState(0);
-  const [currentTime, setCurrentTime] = useState();
+  const [currentTime, setCurrentTime] = useState(0);
 
   // https://reactjs.org/docs/hooks-faq.html#how-can-i-measure-a-dom-node
-  const setRef = useCallback(
+  const ref = useCallback(
     (node: HTMLAudioElement) => {
       if (node) {
         const load = () => setDuration(node.duration);
+        const timeUpdate = () => setCurrentTime(node.currentTime);
         const toggle = () => {
           if (isPlaying) {
             node.play();
@@ -18,8 +27,6 @@ export const usePlayer = () => {
           }
         };
         toggle();
-
-        const timeUpdate = () => setCurrentTime(String(node.currentTime));
 
         node.addEventListener('loadeddata', load);
         node.addEventListener('timeupdate', timeUpdate);
@@ -32,5 +39,5 @@ export const usePlayer = () => {
     },
     [isPlaying]
   );
-  return [setRef, setIsPlaying, isPlaying, currentTime, duration];
+  return { ref, setIsPlaying, isPlaying, currentTime, duration };
 };
