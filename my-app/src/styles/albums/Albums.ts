c418a96e58dab8455652363dc9abc125bdf1aa63
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-03-09 16:35:13 -0600

    The exact size

--- my-app/src/styles/albums/Albums.ts
+++ my-app/src/styles/albums/Albums.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 250px);
+  grid-template-columns: repeat(auto-fill, 239px);
   grid-gap: 25px;
 `;
--- my-app/src/styles/artists/Artists.ts
+++ my-app/src/styles/artists/Artists.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 264px);
+  grid-template-columns: repeat(auto-fill, 247px);
   grid-gap: 15px;
 `;
