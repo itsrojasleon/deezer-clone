7a5606d1d2bd28e8956bbeae967161f4e49966d0
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-05-07 19:43:31 -0500

    A little more

--- client/src/styles/shared/gridContainer.ts
+++ client/src/styles/shared/gridContainer.ts
@@ -2,6 +2,6 @@ import { css } from 'styled-components';
 
 export const gridContainer = css`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 247px);
+  grid-template-columns: repeat(auto-fill, 244px);
   grid-gap: 15px;
 `;
